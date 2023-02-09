b444f1e327d891432dd25e47b8903d7d97e0ab3d
Murphy Mbani <marteliembani@gmail.com>
2023-02-10 00:12:44 +0100

    Before trying on mac

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Sub_Scheduler',
+      title: 'SubTracker',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
